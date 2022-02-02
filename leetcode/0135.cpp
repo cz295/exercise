8c5b2af4d23961067b8cad0f90d3b98781b38398
cz295 <zkcheng1994@outlook.com>
2022-02-02 18:18:29 +0000

    0202

---
+++ leetcode/0135.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candies(ratings.size(), 1);
+        for(int i = 1; i < ratings.size(); ++i)
+        {
+            if(ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        }
+        cout << ratings.size();
+        for(int i = ratings.size() - 2; i >= 0; --i)
+        {
+            if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
+                candies[i] = candies[i + 1] + 1;
+        }
+        int sum_candies = 0;
+        for (auto x: candies)
+        { 
+            sum_candies += x;
+        }
+        return sum_candies;
+    }
+};
